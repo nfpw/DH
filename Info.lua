e2890dce6bd3ada4861598338189688c63a1e964
nfpw <153939922+nfpw@users.noreply.github.com>
2025-02-09 12:34:27 +0300

    Update Info.lua

--- Info.lua
+++ Info.lua
@@ -1,6 +1,7 @@
 local Library = getgenv().__Library
 local Window = getgenv().__Window
 local DCLink = "https://discord.gg/NUgDf8cWjr"
+local JobId = game.JobId
 
 local Tabs = {
     Info = Window:AddTab('Info'),
@@ -47,3 +48,34 @@ Info:AddButton('Or join the discord', function()
     setclipboard(DCLink)
 end)
 Info:AddDivider()
+
+local JobID = Tabs.Info:AddRightGroupbox('JobId')
+
+JobID:AddDivider()
+JobID:AddButton('Copy Current JobId', function()
+    setclipboard(game.JobId)
+    NftD("Copied current JobId: "..game.JobId, 2)
+end)
+JobID:AddInput('JobIdInput', {
+    Default = '',
+    Text = 'Enter JobId',
+    Placeholder = JobId,
+    Finished = true,
+    Callback = function(value)
+        NftD("JobId set to: "..tostring(value), 2)
+        JobId = value
+    end,
+})
+JobID:AddButton('Copy Web Join Code', function()
+    local JoinCode = string.format("Roblox.GameLauncher.joinGameInstance(%d, %q)", game.PlaceId, JobId)
+    setclipboard(JoinCode)
+    NftD("Copied web join code: "..JoinCode, 2)
+end)
+JobID:AddButton('Join JobId', function()
+    if JobId and JobId ~= "" then
+        cloneref(game:GetService("TeleportService")):TeleportToPlaceInstance(game.PlaceId, JobId)
+    else
+        NftD("Please enter a valid JobId first", 2)
+    end
+end)
+JobID:AddDivider()
